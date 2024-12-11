c70f9575bc2378ad570bcac7b0cd3e8b71983de3
Alydin <alykulovalydin@gmail.com>
2024-12-11 23:32:45 +0600

    refactored project?

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -17,75 +17,7 @@ public class Controller extends MouseAdapter implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         // Handling button actions for creating, updating, deleting, and reading
-        if (e.getActionCommand().equals("createBook")) {
-            String isbn = viewer.getBookIsbnField().getText();
-            String title = viewer.getBookTitleField().getText();
-            int publicationYear = Integer.parseInt(viewer.getBookYearField().getText());
-            double price = Double.parseDouble(viewer.getBookPriceField().getText());
 
-            model.createBook(isbn, title, publicationYear, price);
-        } else if (e.getActionCommand().equals("updateBook")){
-            String isbn = viewer.getBookIsbnField().getText();
-            String title = viewer.getBookTitleField().getText();
-            int publicationYear = Integer.parseInt(viewer.getBookYearField().getText());
-            double price = Double.parseDouble(viewer.getBookPriceField().getText());
-
-            model.updateBook(isbn, title, publicationYear, price);
-        } else if (e.getActionCommand().equals("deleteBook")){
-            String isbn = viewer.getBookIsbnField().getText();
-            model.deleteBook(isbn);
-        } else if (e.getActionCommand().equals("createAuthor")){
-            String firstName = viewer.getAuthorFirstNameField().getText();
-            String lastName = viewer.getAuthorLastNameField().getText();
-            model.createAuthor(firstName, lastName);
-        } else if (e.getActionCommand().equals("updateAuthor")){
-            int authorId = Integer.parseInt(viewer.getAuthorIdField().getText());
-            String firstName = viewer.getAuthorFirstNameField().getText();
-            String lastName = viewer.getAuthorLastNameField().getText();
-            model.updateAuthor(authorId, firstName, lastName);
-        } else if (e.getActionCommand().equals("deleteAuthor")){
-            int authorId = Integer.parseInt(viewer.getAuthorIdField().getText());
-            model.deleteAuthor(authorId);
-        } else if (e.getActionCommand().equals("createCustomer")){
-            String name = viewer.getCustomerNameField().getText();
-            String address = viewer.getCustomerAddressField().getText();
-            model.createCustomer(name, address);
-        } else if (e.getActionCommand().equals("updateCustomer")){
-            int customerId = Integer.parseInt(viewer.getCustomerIdField().getText());
-            String name = viewer.getCustomerNameField().getText();
-            String address = viewer.getCustomerAddressField().getText();
-            model.updateCustomer(customerId, name, address);
-        } else if (e.getActionCommand().equals("deleteCustomer")){
-            int customerId = Integer.parseInt(viewer.getCustomerIdField().getText());
-            model.deleteCustomer(customerId);
-        } else if (e.getActionCommand().equals("createOrder")){
-            int customerId = Integer.parseInt(viewer.getCustomerIdField().getText());
-            String orderDate = viewer.getOrderDateField().getText();
-            model.createOrder(customerId, orderDate);
-        } else if (e.getActionCommand().equals("updateOrder")){
-            int orderId = Integer.parseInt(viewer.getOrderIdField().getText());
-            int customerId = Integer.parseInt(viewer.getCustomerIdField().getText());
-            String orderDate = viewer.getOrderDateField().getText();
-            model.updateOrder(orderId, orderDate);
-        } else if (e.getActionCommand().equals("deleteOrder")){
-            int orderId = Integer.parseInt(viewer.getOrderIdField().getText());
-            model.deleteOrder(orderId);
-        }
-
-        // Show List actions for books, authors, customers, and orders
-        if (e.getActionCommand().equals("showBookList")) {
-            List<Book> bookList = model.readBooks();
-            viewer.getBookListArea().setText(bookList);  // Displaying book list in JTextArea
-        } else if (e.getActionCommand().equals("showAuthorList")) {
-            String authorList = model.getAuthorList();
-            viewer.getAuthorListArea().setText(authorList);  // Displaying author list in JTextArea
-        } else if (e.getActionCommand().equals("showCustomerList")) {
-            String customerList = model.getCustomerList();
-            viewer.getCustomerListArea().setText(customerList);  // Displaying customer list in JTextArea
-        } else if (e.getActionCommand().equals("showOrderList")) {
-            String orderList = model.getOrderList();
-            viewer.getOrderListArea().setText(orderList);  // Displaying order list in JTextArea
-        }
     }
 
     // This method can be removed if not needed anymore
--- src/main/java/Viewer.java
+++ src/main/java/Viewer.java
@@ -1,291 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class Viewer extends JFrame {
-    private JTextField bookIsbnField, bookTitleField, bookYearField, bookPriceField;
-    private JTextArea bookListArea;
-    private JTextField authorIdField, authorFirstNameField, authorLastNameField;
-    private JTextArea authorListArea;
-    private JTextField customerIdField, customerNameField, customerAddressField;
-    private JTextArea customerListArea;
-    private JTextField orderIdField, customerIdFieldForOrder, orderDateField;
-    private JTextArea orderListArea;
-
-    private JButton createBookButton, updateBookButton, deleteBookButton, showBookListButton;
-    private JButton createAuthorButton, updateAuthorButton, deleteAuthorButton, showAuthorListButton;
-    private JButton createCustomerButton, updateCustomerButton, deleteCustomerButton, showCustomerListButton;
-    private JButton createOrderButton, updateOrderButton, deleteOrderButton, showOrderListButton;
-
+public class Viewer extends JPanel {
+    private ResourceBundle resourceBundle;
+    private Connection connection;
+    private JComboBox<String> options;
     public Viewer() {
-        setTitle("Bookstore Management");
+        loadResources();
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Bookstore Management");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+
         setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        // Panels for book actions
-        JPanel bookPanel = new JPanel();
-        bookPanel.setLayout(new GridLayout(5, 2));
-        bookPanel.add(new JLabel("ISBN:"));
-        bookIsbnField = new JTextField();
-        bookPanel.add(bookIsbnField);
-        bookPanel.add(new JLabel("Title:"));
-        bookTitleField = new JTextField();
-        bookPanel.add(bookTitleField);
-        bookPanel.add(new JLabel("Year:"));
-        bookYearField = new JTextField();
-        bookPanel.add(bookYearField);
-        bookPanel.add(new JLabel("Price:"));
-        bookPriceField = new JTextField();
-        bookPanel.add(bookPriceField);
 
-        createBookButton = new JButton("Create Book");
-        updateBookButton = new JButton("Update Book");
-        deleteBookButton = new JButton("Delete Book");
-        showBookListButton = new JButton("Show Book List");
+        options = new JComboBox<>();
+        fillOptions();
 
-        bookPanel.add(createBookButton);
-        bookPanel.add(updateBookButton);
-        bookPanel.add(deleteBookButton);
-        bookPanel.add(showBookListButton);
-
-        // Panel for book list
-        bookListArea = new JTextArea();
-        bookListArea.setEditable(false);
-        JScrollPane bookScrollPane = new JScrollPane(bookListArea);
-
-        // Panels for author actions
-        JPanel authorPanel = new JPanel();
-        authorPanel.setLayout(new GridLayout(4, 2));
-        authorPanel.add(new JLabel("Author ID:"));
-        authorIdField = new JTextField();
-        authorPanel.add(authorIdField);
-        authorPanel.add(new JLabel("First Name:"));
-        authorFirstNameField = new JTextField();
-        authorPanel.add(authorFirstNameField);
-        authorPanel.add(new JLabel("Last Name:"));
-        authorLastNameField = new JTextField();
-        authorPanel.add(authorLastNameField);
-
-        createAuthorButton = new JButton("Create Author");
-        updateAuthorButton = new JButton("Update Author");
-        deleteAuthorButton = new JButton("Delete Author");
-        showAuthorListButton = new JButton("Show Author List");
-
-        authorPanel.add(createAuthorButton);
-        authorPanel.add(updateAuthorButton);
-        authorPanel.add(deleteAuthorButton);
-        authorPanel.add(showAuthorListButton);
-
-        // Panel for author list
-        authorListArea = new JTextArea();
-        authorListArea.setEditable(false);
-        JScrollPane authorScrollPane = new JScrollPane(authorListArea);
-
-        // Panels for customer actions
-        JPanel customerPanel = new JPanel();
-        customerPanel.setLayout(new GridLayout(4, 2));
-        customerPanel.add(new JLabel("Customer ID:"));
-        customerIdField = new JTextField();
-        customerPanel.add(customerIdField);
-        customerPanel.add(new JLabel("Name:"));
-        customerNameField = new JTextField();
-        customerPanel.add(customerNameField);
-        customerPanel.add(new JLabel("Address:"));
-        customerAddressField = new JTextField();
-        customerPanel.add(customerAddressField);
-
-        createCustomerButton = new JButton("Create Customer");
-        updateCustomerButton = new JButton("Update Customer");
-        deleteCustomerButton = new JButton("Delete Customer");
-        showCustomerListButton = new JButton("Show Customer List");
-
-        customerPanel.add(createCustomerButton);
-        customerPanel.add(updateCustomerButton);
-        customerPanel.add(deleteCustomerButton);
-        customerPanel.add(showCustomerListButton);
-
-        // Panel for customer list
-        customerListArea = new JTextArea();
-        customerListArea.setEditable(false);
-        JScrollPane customerScrollPane = new JScrollPane(customerListArea);
-
-        // Panels for order actions
-        JPanel orderPanel = new JPanel();
-        orderPanel.setLayout(new GridLayout(4, 2));
-        orderPanel.add(new JLabel("Order ID:"));
-        orderIdField = new JTextField();
-        orderPanel.add(orderIdField);
-        orderPanel.add(new JLabel("Customer ID:"));
-        customerIdFieldForOrder = new JTextField();
-        orderPanel.add(customerIdFieldForOrder);
-        orderPanel.add(new JLabel("Order Date:"));
-        orderDateField = new JTextField();
-        orderPanel.add(orderDateField);
-
-        createOrderButton = new JButton("Create Order");
-        updateOrderButton = new JButton("Update Order");
-        deleteOrderButton = new JButton("Delete Order");
-        showOrderListButton = new JButton("Show Order List");
-
-        orderPanel.add(createOrderButton);
-        orderPanel.add(updateOrderButton);
-        orderPanel.add(deleteOrderButton);
-        orderPanel.add(showOrderListButton);
-
-        // Panel for order list
-        orderListArea = new JTextArea();
-        orderListArea.setEditable(false);
-        JScrollPane orderScrollPane = new JScrollPane(orderListArea);
-
-        // Adding all panels and areas to the frame
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new GridLayout(2, 2));
-        topPanel.add(bookPanel);
-        topPanel.add(authorPanel);
-        topPanel.add(customerPanel);
-        topPanel.add(orderPanel);
-
-        add(topPanel, BorderLayout.NORTH);
-        add(bookScrollPane, BorderLayout.WEST);
-        add(authorScrollPane, BorderLayout.CENTER);
-        add(customerScrollPane, BorderLayout.EAST);
-        add(orderScrollPane, BorderLayout.SOUTH);
-
-        setVisible(true);
+        frame.add(options, BorderLayout.NORTH);
+        frame.setVisible(true);
     }
 
-    public JTextField getBookIsbnField() {
-        return bookIsbnField;
+
+    public void loadResources() {
+        resourceBundle = ResourceBundle.getBundle("application");
+        String URL = resourceBundle.getString("URL");
+        String USER = resourceBundle.getString("USER");
+        String PASSWORD = resourceBundle.getString("PASSWORD");
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            System.out.println("Connection failed" + e.getMessage());
+        }
     }
 
-    public JTextField getBookTitleField() {
-        return bookTitleField;
-    }
+    private void fillOptions() {
+        options.addItem("Create book");
+        options.addItem("Update book");
+        options.addItem("Delete book");
+        options.addItem("Show all books");
 
-    public JTextField getBookYearField() {
-        return bookYearField;
-    }
+        options.addItem("Create customer");
+        options.addItem("Update customer");
+        options.addItem("Delete customer");
+        options.addItem("Show all customers");
 
-    public JTextField getBookPriceField() {
-        return bookPriceField;
-    }
+        options.addItem("Create order");
+        options.addItem("Update order");
+        options.addItem("Delete order");
+        options.addItem("Show all orders");
 
-    public JTextArea getBookListArea() {
-        return bookListArea;
-    }
+        options.addItem("Retrieve a list of books with their authors.");
+        options.addItem("Find the total sales for a specific book.");
+        options.addItem("List customers who made more than one purchase.");
+        options.addItem("Find the top N best-selling books.");
+        options.addItem("Identify authors with the highest total sales.");
+        options.addItem("Retrieve customers with the highest total purchase amount.");
+        options.addItem("Identify books with low stock.");
+        options.addItem("Retrieve the latest orders placed.");
+        options.addItem("Calculate total revenue.");
+        options.addItem("Identify customers who have not made a purchase.");
 
-    public JTextField getAuthorIdField() {
-        return authorIdField;
-    }
 
-    public JTextField getAuthorFirstNameField() {
-        return authorFirstNameField;
-    }
-
-    public JTextField getAuthorLastNameField() {
-        return authorLastNameField;
-    }
-
-    public JTextArea getAuthorListArea() {
-        return authorListArea;
-    }
-
-    public JTextField getCustomerIdField() {
-        return customerIdField;
-    }
-
-    public JTextField getCustomerNameField() {
-        return customerNameField;
-    }
-
-    public JTextField getCustomerAddressField() {
-        return customerAddressField;
-    }
-
-    public JTextArea getCustomerListArea() {
-        return customerListArea;
-    }
-
-    public JTextField getOrderIdField() {
-        return orderIdField;
-    }
-
-    public JTextField getCustomerIdFieldForOrder() {
-        return customerIdFieldForOrder;
-    }
-
-    public JTextField getOrderDateField() {
-        return orderDateField;
-    }
-
-    public JTextArea getOrderListArea() {
-        return orderListArea;
-    }
-
-    // Methods for adding action listeners
-    public void addCreateBookListener(ActionListener listener) {
-        createBookButton.addActionListener(listener);
-    }
-
-    public void addUpdateBookListener(ActionListener listener) {
-        updateBookButton.addActionListener(listener);
-    }
-
-    public void addDeleteBookListener(ActionListener listener) {
-        deleteBookButton.addActionListener(listener);
-    }
-
-    public void addShowBookListListener(ActionListener listener) {
-        showBookListButton.addActionListener(listener);
-    }
-
-    public void addCreateAuthorListener(ActionListener listener) {
-        createAuthorButton.addActionListener(listener);
-    }
-
-    public void addUpdateAuthorListener(ActionListener listener) {
-        updateAuthorButton.addActionListener(listener);
-    }
-
-    public void addDeleteAuthorListener(ActionListener listener) {
-        deleteAuthorButton.addActionListener(listener);
-    }
-
-    public void addShowAuthorListListener(ActionListener listener) {
-        showAuthorListButton.addActionListener(listener);
-    }
-
-    public void addCreateCustomerListener(ActionListener listener) {
-        createCustomerButton.addActionListener(listener);
-    }
-
-    public void addUpdateCustomerListener(ActionListener listener) {
-        updateCustomerButton.addActionListener(listener);
-    }
-
-    public void addDeleteCustomerListener(ActionListener listener) {
-        deleteCustomerButton.addActionListener(listener);
-    }
-
-    public void addShowCustomerListListener(ActionListener listener) {
-        showCustomerListButton.addActionListener(listener);
-    }
-
-    public void addCreateOrderListener(ActionListener listener) {
-        createOrderButton.addActionListener(listener);
-    }
-
-    public void addUpdateOrderListener(ActionListener listener) {
-        updateOrderButton.addActionListener(listener);
-    }
-
-    public void addDeleteOrderListener(ActionListener listener) {
-        deleteOrderButton.addActionListener(listener);
-    }
-
-    public void addShowOrderListListener(ActionListener listener) {
-        showOrderListButton.addActionListener(listener);
     }
 }
